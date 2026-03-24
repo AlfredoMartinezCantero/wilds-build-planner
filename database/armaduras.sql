8c041e9aecddcb96bbb9a7abc261860f564efbe3
AlfredoMartinezCantero <alfredomartinezcantero@gmail.com>
2026-03-24 18:38:57 +0100

    1

--- database/armaduras.sql
+++ database/armaduras.sql
@@ -21,9 +21,210 @@ CREATE INDEX idx_armaduras_nombre      ON armaduras(nombre);
 
 ---------------
 
-INSERT INTO armaduras (nombre, tipo, rareza, defensa, elem_fuego, elem_agua, elem_rayo, elem_hielo, elem_dragon, slots) VALUES
-('Casco de Cazador',     'head',  1, 20, 0, 0, 0, 0, 0, 1),
-('Pechera de Cazador',   'chest', 1, 22, 0, 0, 0, 0, 0, 1),
-('Brazales de Cazador',  'gloves',1, 18, 0, 0, 0, 0, 0, 1),
-('Faja de Cazador',      'waist', 1, 19, 0, 0, 0, 0, 0, 1),
-('Grebas de Cazador',    'legs',  1, 21, 0, 0, 0, 0, 0, 1);
+-- LEATHER (Rarity 1)
+INSERT INTO armaduras (nombre,tipo,rareza) VALUES
+('Leather Headgear','head',1),
+('Leather Vest','chest',1),
+('Leather Gloves','gloves',1),
+('Leather Belt','waist',1),
+('Leather Pants','legs',1);
+
+-- CHAINMAIL (Rarity 1)
+INSERT INTO armaduras (nombre,tipo,rareza) VALUES
+('Chainmail Headgear','head',1),
+('Chainmail Vest','chest',1),
+('Chainmail Gloves','gloves',1),
+('Chainmail Belt','waist',1),
+('Chainmail Pants','legs',1);
+
+-- BONE (Rarity 1)
+INSERT INTO armaduras (nombre,tipo,rareza) VALUES
+('Bone Helm','head',1),
+('Bone Mail','chest',1),
+('Bone Vambraces','gloves',1),
+('Bone Coil','waist',1),
+('Bone Greaves','legs',1);
+
+-- HOPE (Rarity 1)
+INSERT INTO armaduras (nombre,tipo,rareza) VALUES
+('Hope Mask','head',1),
+('Hope Mail','chest',1),
+('Hope Vambraces','gloves',1),
+('Hope Coil','waist',1),
+('Hope Greaves','legs',1);
+
+-- TALIOTH (Rarity 1)
+INSERT INTO armaduras (nombre,tipo,rareza) VALUES
+('Talioth Mask','head',1),
+('Talioth Mail','chest',1),
+('Talioth Vambraces','gloves',1),
+('Talioth Coil','waist',1),
+('Talioth Greaves','legs',1);
+
+-- PIRAGILL (Rarity 1)
+INSERT INTO armaduras (nombre,tipo,rareza) VALUES
+('Piragill Helm','head',1),
+('Piragill Mail','chest',1),
+('Piragill Vambraces','gloves',1),
+('Piragill Coil','waist',1),
+('Piragill Greaves','legs',1);
+
+-- VESPOID (Rarity 1)
+INSERT INTO armaduras (nombre,tipo,rareza) VALUES
+('Vespoid Helm','head',1),
+('Vespoid Mail','chest',1),
+('Vespoid Vambraces','gloves',1),
+('Vespoid Coil','waist',1),
+('Vespoid Greaves','legs',1);
+
+-- BULAQCHI (Rarity 1)
+INSERT INTO armaduras (nombre,tipo,rareza) VALUES
+('Bulaqchi Specs','head',1),
+('Bulaqchi Mail','chest',1),
+('Bulaqchi Vambraces','gloves',1),
+('Bulaqchi Coil','waist',1),
+('Bulaqchi Greaves','legs',1);
+
+-- CONGA (Rarity 2)
+INSERT INTO armaduras (nombre,tipo,rareza) VALUES
+('Conga Helm','head',2),
+('Conga Mail','chest',2),
+('Conga Vambraces','gloves',2),
+('Conga Coil','waist',2),
+('Conga Greaves','legs',2);
+
+-- BALAHARA (Rarity 2)
+INSERT INTO armaduras (nombre,tipo,rareza) VALUES
+('Balahara Helm','head',2),
+('Balahara Mail','chest',2),
+('Balahara Vambraces','gloves',2),
+('Balahara Coil','waist',2),
+('Balahara Greaves','legs',2);
+
+-- DOSHAGUMA (Rarity 2)
+INSERT INTO armaduras (nombre,tipo,rareza) VALUES
+('Doshaguma Helm','head',2),
+('Doshaguma Mail','chest',2),
+('Doshaguma Braces','gloves',2),
+('Doshaguma Coil','waist',2),
+('Doshaguma Greaves','legs',2);
+
+-- AJARAKAN (Rarity 3)
+INSERT INTO armaduras (nombre,tipo,rareza) VALUES
+('Ajarakan Helm','head',3),
+('Ajarakan Mail','chest',3),
+('Ajarakan Vambraces','gloves',3),
+('Ajarakan Coil','waist',3),
+('Ajarakan Greaves','legs',3);
+
+-- COMAQCHI (Rarity 3)
+INSERT INTO armaduras (nombre,tipo,rareza) VALUES
+('Comaqchi Helm','head',3),
+('Comaqchi Mail','chest',3),
+('Comaqchi Vambraces','gloves',3),
+('Comaqchi Coil','waist',3),
+('Comaqchi Greaves','legs',3);
+
+-- HIRABAMI (Rarity 3)
+INSERT INTO armaduras (nombre,tipo,rareza) VALUES
+('Hirabami Headdress','head',3),
+('Hirabami Mail','chest',3),
+('Hirabami Vambraces','gloves',3),
+('Hirabami Coil','waist',3),
+('Hirabami Greaves','legs',3);
+
+-- INGOT (Rarity 3)
+INSERT INTO armaduras (nombre,tipo,rareza) VALUES
+('Ingot Helm','head',3),
+('Ingot Mail','chest',3),
+('Ingot Vambraces','gloves',3),
+('Ingot Coil','waist',3),
+('Ingot Greaves','legs',3);
+
+-- ROMPOPOLO (Rarity 3)
+INSERT INTO armaduras (nombre,tipo,rareza) VALUES
+('Rompopolo Helm','head',3),
+('Rompopolo Mail','chest',3),
+('Rompopolo Vambraces','gloves',3),
+('Rompopolo Coil','waist',3),
+('Rompopolo Greaves','legs',3);
+
+-- NERSCYLLA (Rarity 3)
+INSERT INTO armaduras (nombre,tipo,rareza) VALUES
+('Nerscylla Helm','head',3),
+('Nerscylla Mail','chest',3),
+('Nerscylla Vambraces','gloves',3),
+('Nerscylla Coil','waist',3),
+('Nerscylla Greaves','legs',3);
+
+-- KRANODATH (Rarity 3)
+INSERT INTO armaduras (nombre,tipo,rareza) VALUES
+('Kranodath Helm','head',3),
+('Kranodath Mail','chest',3),
+('Kranodath Vambraces','gloves',3),
+('Kranodath Coil','waist',3),
+('Kranodath Greaves','legs',3);
+
+-- GUARDIAN DOSHAGUMA (Rarity 4)
+INSERT INTO armaduras (nombre,tipo,rareza) VALUES
+('G. Doshaguma Helm','head',4),
+('G. Doshaguma Mail','chest',4),
+('G. Doshaguma Braces','gloves',4),
+('G. Doshaguma Coil','waist',4),
+('G. Doshaguma Greaves','legs',4);
+
+-- UTH DUNA (Rarity 4)
+INSERT INTO armaduras (nombre,tipo,rareza) VALUES
+('Uth Duna Helm','head',4),
+('Uth Duna Mail','chest',4),
+('Uth Duna Vambraces','gloves',4),
+('Uth Duna Coil','waist',4),
+('Uth Duna Greaves','legs',4);
+
+-- REY DAU (Rarity 4)
+INSERT INTO armaduras (nombre,tipo,rareza) VALUES
+('Rey Dau Helm','head',4),
+('Rey Dau Mail','chest',4),
+('Rey Dau Vambraces','gloves',4),
+('Rey Dau Coil','waist',4),
+('Rey Dau Greaves','legs',4);
+
+-- NU UDRA (Rarity 4)
+INSERT INTO armaduras (nombre,tipo,rareza) VALUES
+('Nu Udra Helm','head',4),
+('Nu Udra Mail','chest',4),
+('Nu Udra Vambraces','gloves',4),
+('Nu Udra Coil','waist',4),
+('Nu Udra Greaves','legs',4);
+
+-- GUARDIAN EBONY (Rarity 4)
+INSERT INTO armaduras (nombre,tipo,rareza) VALUES
+('G. Ebony Helm','head',4),
+('G. Ebony Mail','chest',4),
+('G. Ebony Braces','gloves',4),
+('G. Ebony Coil','waist',4),
+('G. Ebony Greaves','legs',4);
+
+-- XU WU (Rarity 4)
+INSERT INTO armaduras (nombre,tipo,rareza) VALUES
+('Xu Wu Helm','head',4),
+('Xu Wu Mail','chest',4),
+('Xu Wu Vambraces','gloves',4),
+('Xu Wu Coil','waist',4),
+('Xu Wu Greaves','legs',4);
+
+-- GUARDIAN ARKVELD (Rarity 4)
+INSERT INTO armaduras (nombre,tipo,rareza) VALUES
+('G. Arkveld Helm','head',4),
+('G. Arkveld Mail','chest',4),
+('G. Arkveld Vambraces','gloves',4),
+('G. Arkveld Coil','waist',4),
+('G. Arkveld Greaves','legs',4);
+
+-- GUARDIAN RATHALOS (Rarity 4)
+INSERT INTO armaduras (nombre,tipo,rareza) VALUES
+('G. Rathalos Helm','head',4),
+('G. Rathalos Mail','chest',4),
+('G. Rathalos Vambraces','gloves',4),
+('G. Rathalos Coil','waist',4),
+('G. Rathalos Greaves','legs',4);
--- database/armas.sql
+++ database/armas.sql
@@ -31,3 +31,166 @@ INSERT INTO armas (nombre, tipo, rareza, ataque, elemento, afinidad, defensa) VA
 ('Ballesta Ligera',   'lbg',           1,  75, NULL,  0, 0),
 ('Ballesta Pesada',   'hbg',           1,  85, NULL,  0, 0),
 ('Arco',              'bow',           1,  80, NULL,  0, 0);
+
+-- GREAT SWORD (oficiales)
+INSERT INTO armas (nombre,tipo) VALUES
+('Sieglinde','great_sword'),
+('Rooster Decapitator','great_sword'),
+('Prinvrilo''s Dissolution','great_sword'),
+('Firetrail Blaisdell','great_sword'),
+('Ravager Blade','great_sword'),
+('Precipice Metallam','great_sword'),
+('Whitefire Rathguard','great_sword'),
+('Rathalos Firesword','great_sword'),
+('Immolator Blade','great_sword'),
+('Düsterstolz','great_sword'),
+('Bone Slasher','great_sword'),
+('Abaddonian Krake','great_sword'),
+('Poison King','great_sword'),
+('Grimslayer Urgeom','great_sword'),
+('Fulgurcleaver Guardiana','great_sword'),
+('G. Stalwart Lamorak','great_sword'),
+('Freezer Speartuna','great_sword'),
+('Destructive Torpor','great_sword'),
+('Varianza','great_sword'),
+('Esperanza Blade','great_sword');
+
+-- LONG SWORD (oficiales)
+INSERT INTO armas (nombre,tipo) VALUES
+('Akanesasu','long_sword'),
+('Fellslayer Dangeom','long_sword'),
+('Stalwart Lamorak','long_sword'),
+('Giant Jawblade','long_sword'),
+('Blazing Veiah','long_sword'),
+('Hope Blade V','long_sword'),
+('Valkyrie Blade II','long_sword'),
+('Chicken Decapitator II','long_sword'),
+('Quematrice Espada IV','long_sword'),
+('Jin Dhavaar I','long_sword'),
+('Immane Blade III','long_sword'),
+('Schattenstolz I','long_sword'),
+('Nu Krake II','long_sword'),
+('Veldian Gladius I','long_sword'),
+('Paretic Blade IV','long_sword'),
+('Artian Blade II','long_sword'),
+('Zoh Veiah I','long_sword'),
+('Nihil Great Sword II','long_sword'),
+('Buster Sword III','long_sword'),
+('Albirath Wing II','long_sword');
+
+-- SWORD & SHIELD (oficiales)
+INSERT INTO armas (nombre,tipo) VALUES
+('Red Wing I','sword_shield'),
+('Bone Blade IV','sword_shield'),
+('Slaughter II','sword_shield'),
+('Hope Edge I','sword_shield'),
+('Quematrice Edge II','sword_shield'),
+('Gypceros Knives I','sword_shield'),
+('Kut-Ku Pick I','sword_shield'),
+('Gravios Scrapper I','sword_shield'),
+('Rathalos Edge I','sword_shield'),
+('Arkveld Shortblade I','sword_shield');
+
+-- DUAL BLADES (oficiales)
+INSERT INTO armas (nombre,tipo) VALUES
+('Twin Flames I','dual_blades'),
+('Nerscylla Fangs I','dual_blades'),
+('Hirabami Cutters I','dual_blades'),
+('Rompopolo Claws I','dual_blades'),
+('Ajarakan Daggers I','dual_blades'),
+('Balahara Skinners I','dual_blades'),
+('Doshaguma Talons I','dual_blades'),
+('Nu Udra Rippers I','dual_blades'),
+('Rey Dau Twinblades I','dual_blades'),
+('Uth Duna Slashers I','dual_blades');
+
+INSERT INTO armas (nombre, tipo) VALUES
+('Chatacabra Longblade','long_sword'),
+('Balahara Carver','long_sword'),
+('Uth Duna Edge','long_sword'),
+('Nu Udra Piercer','long_sword'),
+('Rey Dau Royal Katana','long_sword');
+
+INSERT INTO armas (nombre, tipo) VALUES
+('Rathalos Buckblade','sword_shield'),
+('Nerscylla Cutter Shield','sword_shield'),
+('Ajarakan Guardblade','sword_shield'),
+('Gravios Towerblade','sword_shield'),
+('Lala Barina Crest Shield','sword_shield');
+
+INSERT INTO armas (nombre, tipo) VALUES
+('Kut-Ku Twin Talons','dual_blades'),
+('Nerscylla Slicers','dual_blades'),
+('Hirabami Wingblades','dual_blades'),
+('Balahara Whirlblades','dual_blades'),
+('Doshaguma Claws','dual_blades');
+
+INSERT INTO armas (nombre, tipo) VALUES
+('Gravios Breakmaul','hammer'),
+('Chatacabra Thumpdrum','hammer'),
+('Rey Dau Crowncrusher','hammer'),
+('Uth Duna Impact Horn','hammer'),
+('Nerscylla Skullbash','hammer');
+
+INSERT INTO armas (nombre, tipo) VALUES
+('Lala Barina Serenade','hunting_horn'),
+('Gore Symphony','hunting_horn'),
+('Ajarakan Chant','hunting_horn'),
+('Nu Udra Resonant Horn','hunting_horn'),
+('Balahara Echo Drum','hunting_horn');
+
+INSERT INTO armas (nombre, tipo) VALUES
+('Guardian Rathalos Lance','lance'),
+('Arkveld Guardpiercer','lance'),
+('Rompopolo Spear','lance'),
+('Chatacabra Impaler','lance'),
+('Doshaguma Fanglance','lance');
+
+INSERT INTO armas (nombre, tipo) VALUES
+('Rey Dau Mortarlance','gunlance'),
+('Nu Udra Burstlance','gunlance'),
+('Gravios Slamcannon','gunlance'),
+('Kut-Ku Flarebeak','gunlance'),
+('Quematrice Blastlance','gunlance');
+
+INSERT INTO armas (nombre, tipo) VALUES
+('Nerscylla Morphslicer','switch_axe'),
+('Balahara Switchcleaver','switch_axe'),
+('Rey Dau Kingsplitter','switch_axe'),
+('Arkveld Fissure Axe','switch_axe'),
+('Gypceros Flash Axe','switch_axe');
+
+INSERT INTO armas (nombre, tipo) VALUES
+('Nu Udra Shieldbinder','charge_blade'),
+('Uth Duna Voltblade','charge_blade'),
+('Arkveld Corecleaver','charge_blade'),
+('Quematrice Ignition Guard','charge_blade'),
+('Rey Dau Royal Phalanx','charge_blade');
+
+INSERT INTO armas (nombre, tipo) VALUES
+('Nerscylla Glaive','insect_glaive'),
+('Hirabami Skytracer','insect_glaive'),
+('Chatacabra Glidepole','insect_glaive'),
+('Uth Duna Leapglaive','insect_glaive'),
+('Nu Udra Stormstaff','insect_glaive');
+
+INSERT INTO armas (nombre, tipo) VALUES
+('Balahara Quickshot','lbg'),
+('Nerscylla Venom LBG','lbg'),
+('Kut-Ku Featherbolt','lbg'),
+('Lala Barina Scout Gun','lbg'),
+('Quematrice Ember LBG','lbg');
+
+INSERT INTO armas (nombre, tipo) VALUES
+('Gravios Howitzer','hbg'),
+('Rey Dau Siege Cannon','hbg'),
+('Balahara Burrower HBG','hbg'),
+('Uth Duna Thunder Roar','hbg'),
+('Arkveld Rupture Cannon','hbg');
+
+INSERT INTO armas (nombre, tipo) VALUES
+('Kut-Ku Wingbow','bow'),
+('Nerscylla Silkbow','bow'),
+('Lala Barina Heartstring','bow'),
+('Nu Udra Stormbow','bow'),
+('Rey Dau Regal Bow','bow');
